0ecb00b702a5e5cd46da49ddbbaea97fddafd2fb
Clifford Grimmell and Josh Linton <clifford@>
2017-03-01 14:04:42 -0800

    add event listener for canvas listens to canvas and returns what row & col user clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-//Back End
+// //Back End
+var turns = ["X","O","X","O","X","O","X","O","X"]
+
 function Player1 (name, position){
   this.playerName = name;
   this.position = position;
@@ -8,8 +10,11 @@ function Player2(name, position){
   this.position = position;
 }
 function Board (){
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
+  var canvas = document.getElementById("canvas"),
+      canvasLeft = canvas.offsetLeft,
+      canvasTop = canvas.offsetTop,
+      ctx = canvas.getContext("2d");
+
   ctx.beginPath();
   ctx.moveTo(200, 0);
   ctx.lineTo(200, 600);
@@ -21,6 +26,69 @@ function Board (){
   ctx.lineTo(600,400);
   ctx.strokeStyle = "red";
   ctx.stroke();
+
+  // get mouse position
+  function getMousePos(ctx,evt){
+    var rect =  canvas.getBoundingCLientRect();
+    return { x: evt.clientX - rect.left, y: evt.clientY - rect.top};
+  };
+  canvas.addEventListener('click', function(event){
+    var x = event.pageX - canvas.offsetLeft,
+        y = event.pageY - canvas.offsetTop;
+
+    if (x < 200) {
+      if (y < 200){
+        console.log("a1")
+      } else if (y > 200 && y <400) {
+        console.log("b1")
+      } else if (y > 400) {
+        console.log("c1")
+      }
+
+    } else if (x > 200 && x < 400) {
+      if (y < 200){
+        console.log("a2")
+      } else if (y > 200 && y < 400) {
+        console.log("b2")
+      } else if (y > 400) {
+        console.log("c2")
+      }
+    } else if (x > 400) {
+      if (y < 200){
+        console.log("a3")
+      } else if (y > 200 && y <400) {
+        console.log("b3")
+      } else if (y > 400) {
+        console.log("c3")
+      }
+    }
+  });
+
+  // ctx.addEventListener('mousemove')
+  //Draw X Function
+  function drawX(x,y){
+    var imageObj = new Image();
+    imageObj.onload = function() {
+      ctx.drawImage(this, x+10, y+10);
+    };
+    imageObj.src = "../tictac/img/x.png";
+  };
+
+  //Draw O Function
+  function drawO(x,y){
+    var imageObj = new Image();
+    imageObj.onload = function() {
+      ctx.drawImage(this, x + 10, y + 10);
+    };
+    imageObj.src = "../tictac/img/objecto.png";
+  };
+
+  drawX(0,200);
+  drawX(200,0);
+  drawO(200,400);
+
+
+
 }
 function UpdateGame(){
 //PLAYER X GOES FIRST
