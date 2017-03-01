a65f7543e9a67e001417d7e991dd70456d71ce27
Clifford Grimmell and Josh Linton <josh@>
2017-03-01 09:48:10 -0800

    Add styling and canvas added custom css styling for basic function of the page added a canvas and was able to draw it to the screen add form for user to input their name

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//Back End
+
+
+
+
+
+
+
+
+
+
+
+//Front End
+$(document).ready(function(){
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  ctx.font = "30px Arial";
+})
