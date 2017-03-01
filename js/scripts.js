ce3f4a3064ead224cae1c03301dea98fc29a68ba
Clifford Grimmell and Josh Linton <josh@>
2017-03-01 10:41:20 -0800

    add function for game start assigment User 1 and User 2 get assigned to player X or O.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,38 @@
 //Back End
-
-
-
-
-
-
-
-
-
-
-
-//Front End
-$(document).ready(function(){
+function Player1 (name, position){
+  this.playerName = name;
+  this.position = position;
+}
+function Player2(name, position){
+  this.playerName = name;
+  this.position = position;
+}
+function Board (){
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  ctx.font = "30px Arial";
+}
+function UpdateGame(){
+//player1 goes first
+if (Player1Name.position === "X")
+//click to draw X
+//player2 goes next
+//click to draw O
+}
+function GameStart(name1,name2){
+  var position1 = "X";
+  var position2 = "O";
+  var player1Name = new Player1(name1, position1);
+  var player2Name = new Player2(name2, position2);
+  console.log(player1Name);
+  console.log(player2Name);
+}
+//Front End
+$(function(){
+  $("form#player").submit(function(event){
+    event.preventDefault();
+    var inputPlayer1 = $("input#player1").val();
+    var inputPlayer2 = $("input#player2").val();
+    GameStart(inputPlayer1, inputPlayer2);
+    Board();
+  })
 })
